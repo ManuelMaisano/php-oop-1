88de95dfdd90a46ccc131a99ca16cb1d970cbf0f
Manuel Maisano <manumaisano@gmail.com>
2024-05-12 22:19:20 +0200

    index structure

--- index.php
+++ index.php
@@ -7,4 +7,11 @@ include __DIR__ ."/Models/Movie.php";
 <main class="container">
 
 <h2>main</h2>
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <a href="#" class="btn btn-primary">hello</a>
+  </div>
+</div>
 </main>
